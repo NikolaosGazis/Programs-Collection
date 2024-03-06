34991fa1fe0632b22c71fd4e5e11c14e07266f49
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-03-06 16:49:06 +0200

    Syntax and changes and addition of comments.

--- url_shortener.py
+++ url_shortener.py
@@ -1,17 +1,18 @@
 # Libraries # 
 import pyshorteners
 
-# Input the URL to be shortend #
-url = input('Enter the URL: ')
-
 # Functions #
 def urlshortener(url):
-    s =  pyshorteners.Shortener()
+    s = pyshorteners.Shortener()
     print(s.tinyurl.short(url))
 
 # Main #
 def main():
+    # Input the URL to be shortend #
+    url = input("Enter the URL you want to be shortened: ")
+    
     urlshortener(url)
 
+# Execute the program #
 if __name__ == '__main__':
     main()
