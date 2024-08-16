9e539ffc0ecf6cdec38e9b84405cc3c08b42d0cd
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-16 04:29:11 +0300

    Update url_shortener.py

--- url_shortener.py
+++ url_shortener.py
@@ -1,17 +1,45 @@
 # Libraries # 
+import sys
 import pyshorteners
 
-# Functions #
-def urlshortener(url):
-    s = pyshorteners.Shortener()
-    print(s.tinyurl.short(url))
+# Classes/Functions #
+class URLShortener:
+    def __init__(self):
+        self.shortener = pyshorteners.Shortener()
+
+    def short_url(self, url, service='tinyurl'):
+        try:
+            if service == 'tinyurl':
+                return self.shortener.tinyurl.short(url)
+            elif service == 'bitly':
+                return self.shortener.bitly.short(url)
+            elif service == 'isgd':
+                return self.shortener.isgd.short(url)
+            else:
+                raise ValueError("[SYSTEM] Service selected is not compactible!")
+        except Exception as e:
+            return f"[SYSTEM] Error: {str(e)}"
+
+def menu():
+    print("--- URL Shortener Menu: ---")
+    print("1. Shorten a URL")
+    print("0. Exit")
 
 # Main #
 def main():
-    # Input the URL to be shortend #
-    url = input("Enter the URL you want to be shortened: ")
-    
-    urlshortener(url)
+    menu()
+    choice = input("[SYSTEM] Input your option: ")
+
+    if choice == '1':
+        url = input("Enter the URL: ")
+        service = input("Choose service (tinyurl/bitly/isgd): ").lower()
+        s_url = url_shortener.short_url(url, service)
+        print(f"[SYSTEM] Shortened URL: {shortened_url}")
+    elif choice == '2':
+        print("[SYSTEM] Exiting...")
+        sys.exit()
+    else:
+        print("Invalid input. Try again.")
 
 # Execute the program #
 if __name__ == '__main__':
