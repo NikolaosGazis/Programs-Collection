c92635969af01375d71de36a532a1156499fe9b7
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-17 04:33:05 +0300

    Refactored Code.

--- students_grades_organizer.sh
+++ students_grades_organizer.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-counter=0 # student counter
 
-# Check if a directory parameter is given by the user - check total arguments #
+# Global Variables #
+counter=0 # of Students.
+grades_loc="$HOME/Desktop/grades.txt"
+organized_dir="$HOME/Desktop/organized"
+
+# Check if a directory parameter is given by the user #
 if [ $# -eq 0 ]; then # none given
-    echo "• You must pass a directory as an argument...try again."
-    exit 1 # end
+    echo "[SYSTEM] You must pass a directory as an argument...try again."
+    exit 1
 fi
 
-# Check if the given directory exists or not #
+# Check if the given directory exists #
 if [ ! -d "$1" ]; then # ! = "not"
-    echo "• Attention! Couldn't find the directory. Ending program..."
-    exit 1 # end
+    echo "[SYSTEM] Attention! Couldn't find the directory. Ending program..."
+    exit 1
 fi
 
-# Check if the grades file exists #
-grades_location="$HOME/Desktop/grades.txt"
-
+# If grades.txt exists #
 if [ -e "$grades_location" ]; then
-    echo "• The file already exists and will be recreated!"
+    echo "[SYSTEM] The file already exists and will be recreated!"
     rm $grades_location
 fi
-
-# Create it #
-touch grades.txt
-echo "• Created grades.txt!"
+touch "$grades_loc"
+echo "[SYSTEM] Created grades.txt!"
 
 # Collect student data #
-for student in "$1"/*; do # for every (*) student folder in the given directory
-    counter=$((counter+1)) # increase
-    # location of the report file #
+for student in "$1"/*; do # for every (*) student folder in the given directory.
+    counter=$((counter+1)) # increment counter.
+    
+    # Location of the report file #
     report_file="$student/report.txt"
-    # collect data from the report #
+    
+    # Collect data from the report #
     stu_name=$(awk '{print $1}' "$report_file")
     reg_number=$(awk '{print $2}' "$report_file")
     
@@ -44,30 +46,29 @@ for student in "$1"/*; do # for every (*) student folder in the given directory
     	project1_score=30
     fi
     
-    # Project 2 - again execute to compare numeric data #
+    # Project 2 - execute so it can compare numeric data #
     pro2_result=$(gcc -o "$student/project2" "$student/project2.c" && "$student/project2")
     if [ "$pro2_result" -eq 10 ]; then # full grade
     	project2_score=70
     fi
     
-    # Total score #
+    # Calculate total score #
     total=$((project1_score+project2_score))
     
     # Pass the data to the grades.txt #
     echo "$stu_name $reg_number project1: $project1_score project2: $project2_score total_grade: $total">>"$grades_location"
     
-    # Check if directory exists and act accordingly #
-    organized="$HOME/Desktop/organized"
-    if [ "$counter" -eq 1 ]; then # we are at the first student
+    # Check if directory exists #
+    if [ "$counter" -eq 1 ]; then # first student.
         if [ -d "$organized" ]; then
-    	    echo "• Organized already exists and will be recreated."
-    	    rm -r "$organized" # may contain files, so rm instead or rmdir
+    	    echo "[SYSTEM] Organized already exists and will be recreated."
+    	    rm -r "$organized" # may contain files, so us rm.
         fi
-        mkdir "$organized"
-        echo "• Created organized directory!"
+        mkdir "$organized_dir"
+        echo "[SYSTEM] Created organized directory!"
     fi    
     
-    # Move files to the directory according to user #
+    # Move files to the directory according to user input #
     read -p "Organize the files of the particular student? (y/n): " user_answer # input
     if [ "$user_answer" = "y" ]; then
     	cp "$student/project1.c" "$organized/${stu_name}_${reg_number}_project1.c"
