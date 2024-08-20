8a693bcf92101438c20d08c20c15cc0891137002
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-20 03:40:58 +0300

    Add files via upload

---
+++ odd_even.py
@@ -0,0 +1,13 @@
+# Main #
+def main():
+    number = int(input("[SYSTEM] Enter the number: "))
+    
+    if number % 2 == 0:
+        print("[SYSTEM] Number is even!")
+    else:
+        print("[SYSTEM] Number is odd!")
+
+
+# Execute the program #
+if __name__ == "__main__":
+    main()
