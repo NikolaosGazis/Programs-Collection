a1a2b70070e56e43f686e3d2b72c6e5335882949
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-23 02:14:36 +0300

    Update binDecXchange.c

--- binDecXchange.c
+++ binDecXchange.c
@@ -1,5 +1,6 @@
 // Headers //
 #include<stdio.h>
+#include<math.h>
 
 // Functions //
 int decToBinary() {
@@ -13,6 +14,30 @@ int binToDecimal() {
 // Main //
 int main()
 {
-    // Do something.
+    // Variables //
+    long long number;
+    int choice;
+
+    while (1) {
+        // Input Choice //
+        printf("[SYSTEM] Do you want to convert: (1) Decimal to Binary or (2) Binary to Decimal? (0 for exit)");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 0:
+                printf("[SYSTEM] Exiting...");
+                exit(0);
+            case 1:
+                // Decimal to Binary.
+                break;
+            case 2:
+                // Binary to Decimal.
+                break;
+            default:
+                printf("[SYSTEM] Input was invalid.");
+                break;
+        }
+    }
+    
     return 0;
 }
