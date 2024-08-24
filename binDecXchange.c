93040893f7e6df54b2a86b70b4b9dbdc23df990f
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-24 17:54:58 +0300

    Finish binDecXchange.c

--- binDecXchange.c
+++ binDecXchange.c
@@ -1,14 +1,41 @@
 // Headers //
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
 // Functions //
-int decToBinary() {
-    // Do something.
+void decToBinary(long long number) {
+    int binary[64], i = 0; // 64-bit number;
+
+    if (number == 0) { // Input is 0.
+        printf("[SYSTEM] Equivalent: 0\n");
+    }
+
+    while (number > 0) {
+        binary[i] = number % 2; // Store the remainder.
+        number = number / 2;
+        i++;
+    }
+
+    // Print the number in reverse.
+    printf("[SYSTEM] Binary: ");
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binary[j]);
+    printf("\n");
 }
 
-int binToDecimal() {
-    // Do something.
+long long binToDecimal(long long number) {
+    long long decNumber = 0;
+    int remainder, base = 1;
+
+    while (number > 0) {
+        remainder = number % 10; // Last digit.
+        decNumber += remainder * base;
+        number = number / 10; // Next digit.
+        base = base * 2;
+    }
+
+    return decNumber;
 }
 
 // Main //
@@ -27,11 +54,15 @@ int main()
             case 0:
                 printf("[SYSTEM] Exiting...");
                 exit(0);
-            case 1:
-                // Decimal to Binary.
+            case 1: // Decimal to Binary.
+                printf("[SYSTEM] Give your decimal number: ");
+                scanf("%lld", &number);
+                decToBinary(number);
                 break;
-            case 2:
-                // Binary to Decimal.
+            case 2: // Binary to Decimal.
+                printf("[SYSTEM] Give your binary number: ");
+                scanf("%lld", &number);
+                printf("[SYSTEM] Decimal: %lld\n", binToDecimal(number));
                 break;
             default:
                 printf("[SYSTEM] Input was invalid.");
