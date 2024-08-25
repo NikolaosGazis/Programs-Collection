c1225d0b31cad373b35b28370da28d3b6ef72dc7
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-26 00:40:54 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -3,10 +3,13 @@ import random
 
 # Functions #
 def menu():
-    print("--- Game of 21 - Blackjack ---\n")
-    print("1. Play\n")
-    print("2. Rules\n")
+    print("--- Game of 21 - Blackjack ---")
+    print("1. Play")
+    print("2. Rules")
     print("0. Exit\n")
+    
+def rules():
+    pass
     
 def deck_creation():
     cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -22,8 +25,15 @@ def card_value(card):
     else:
         return int(card)
 
-def hand_value(hand_cards):
-    pass
+def hand_value_calculation(hand):
+    value = sum(card_value(card) for card in hand)
+    aces = hand.count('A')
+    
+    while value > 21 and aces > 0: # Treat Ace's value as 1 so the player doesn't bust.
+        value -= 10
+        aces -= 1
+        
+    return value
 
 # Main #
 def main():
@@ -33,31 +43,57 @@ def main():
     
     # Game Loop #
     while True:
-        if len(deck) < 15: # Deck needs reset.
-            print("[GAME] Deck needs resetting...\n")
-            deck = deck_creation
+        menu()
+        choice = input("[GAME] What do you want to do?")
         
-        # Bet and Balance #
-        initial_bet = int(input("[GAME] Place your opening bet -> "))
-        if initial_bet > player_money:
-            print("[GAME] Your bet exceeds your balance, try again.")
-            continue
+        if choice == '1': # Play.
+            if len(deck) < 15: # Deck needs reset.
+                print("[GAME] Deck needs resetting...\n")
+                deck = deck_creation
+        
+            # Bet and Balance #
+            initial_bet = int(input("[GAME] Place your opening bet:"))
+            if initial_bet > player_money:
+                print(f"[GAME] Your bet exceeds your balance, you currently have {player_money}€!")
+                continue
+            else:
+                player_money -= initial_bet
+                print(f"[GAME] You placed {initial_bet} and your remaining balance is {player_money}€.")
+            
+            # Initial Cards #
+            player_hand = [deck.pop(), deck.pop()]
+            dealer_hand = [deck.pop(), deck.pop()]
+            
+            # Table - Dealer shows only shows one card #
+            print(f"[GAME] Your cards: {player_hand[0]}, {player_hand[1]}")
+            print(f"[GAME] Dealer cards: {dealer_hand[0]}\n")
+            
+            # Player Turn #
+            while True:
+                hand_value = hand_value_calculation(player_hand)
+                print(f"[GAME] Your hand value is: {hand_value}")
+                if hand_value > 21:
+                    print(f"[GAME] You busted!")
+                action = input("[GAME] Do you wish to 'hit' or 'stand'?").lower()
+                if action == 'hit':
+                    pass
+                elif action == 'stand':
+                    break
+                else:
+                    print("[GAME] Input is invalid, type 'hit' or 'stand'.")
+            
+            # Dealer Turn #
+            
+            # Ask the player if he wishes to play again #
+            replay = input("[GAME] Do you want to play again? (yes/no)").lower()
+            if replay != 'yes':
+                break
+        elif choice == '2': # Rules.
+            pass
+        elif choice == '0':
+            print("[GAME] Thank you for playing! Exiting...")
         else:
-            player_money -= initial_bet
-            print(f"[GAME] You placed {initial_bet} and your remaining balance is {player_money}.")
-        
-        # Game #
-        player_hand = []
-        for _ in range(2):
-            card = deck.pop()
-            player_hand.append(card)
-            print(f"[GAME] You drew the following card: {card}")
-        
-        # Calculate hand value #
-        # Ask the player if he uses to draw another card - place more money - stand #
-        # Announce the results #
-        # Ask if the player wants to play again #
-        # Dealer logic #
+            print("[Game] Your input is invalid, try again.")
 
 if __name__ == "__main__":
     main()
