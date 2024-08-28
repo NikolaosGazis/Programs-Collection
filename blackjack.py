f37517a72c19b0f4b7e1ff5e5d3e8553bc6a36aa
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-28 23:36:00 +0300

    Added Dealer segment.

--- blackjack.py
+++ blackjack.py
@@ -44,7 +44,7 @@ def main():
     # Game Loop #
     while True:
         menu()
-        choice = input("[GAME] What do you want to do?")
+        choice = input("[GAME] What do you want to do? -> ")
         
         if choice == '1': # Play.
             if len(deck) < 15: # Deck needs reset.
@@ -52,13 +52,13 @@ def main():
                 deck = deck_creation
         
             # Bet and Balance #
-            initial_bet = int(input("[GAME] Place your opening bet:"))
+            initial_bet = int(input("[GAME] Place your opening bet: "))
             if initial_bet > player_money:
                 print(f"[GAME] Your bet exceeds your balance, you currently have {player_money}€!")
                 continue
             else:
                 player_money -= initial_bet
-                print(f"[GAME] You placed {initial_bet} and your remaining balance is {player_money}€.")
+                print(f"[GAME] You placed {initial_bet}€ and your remaining balance is {player_money}€.")
             
             # Initial Cards #
             player_hand = [deck.pop(), deck.pop()]
@@ -69,26 +69,51 @@ def main():
             print(f"[GAME] Dealer cards: {dealer_hand[0]}\n")
             
             # Player Turn #
+            busted = False
             while True:
                 hand_value = hand_value_calculation(player_hand)
                 print(f"[GAME] Your hand value is: {hand_value}")
                 if hand_value > 21:
-                    print(f"[GAME] You busted!")
+                    print(f"[GAME] You have busted!")
+                    busted = True
+                    break
                 action = input("[GAME] Do you wish to 'hit' or 'stand'?").lower()
                 if action == 'hit':
-                    pass
+                    player_hand.append(deck.pop())
+                    print(f"[GAME] You drew: {player_hand[-1]}")
                 elif action == 'stand':
                     break
                 else:
-                    print("[GAME] Input is invalid, type 'hit' or 'stand'.")
+                    print("[GAME] Input is invalid, type 'hit' or 'stand' -> .")
             
             # Dealer Turn #
-            
+            if not busted: # Dealer can continue if the player is still in.
+                print(f"[GAME] Dealer reveals his card... {dealer_hand[1]}")
+                # Add logic flow #
+                while hand_value_calculation(dealer_hand) < 17:
+                    dealer_hand.append(deck.pop())
+                    print(f"[GAME] Dealer has drew: {dealer_hand[-1]}")
+                d_value = hand_value_calculation(dealer_hand)
+                print(f"[GAME] Dealer's value now is: {d_value}")
+                
+                # Compare Player's and Dealer's hands #
+                if d_value > 21:
+                    print(f"[GAME] Dealer has busted! You win.")
+                    player_money += 2 * initial_bet
+                elif d_value > hand_value:
+                    print("[GAME] Dealer wins! Better luck next.")
+                elif d_value < hand_value:
+                    print("[GAME] You win!")
+                    player_money += 2 * initial_bet
+                else:
+                    print("[GAME] Its a draw.")
+                    player_money += initial_bet
+                
             # Ask the player if he wishes to play again #
-            replay = input("[GAME] Do you want to play again? (yes/no)").lower()
+            replay = input("[GAME] Do you want to play again? (yes/no) -> ").lower()
             if replay != 'yes':
                 break
-        elif choice == '2': # Rules.
+        elif choice == '2': # Rules - Add.
             pass
         elif choice == '0':
             print("[GAME] Thank you for playing! Exiting...")
