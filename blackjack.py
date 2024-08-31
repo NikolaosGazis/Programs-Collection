5c2eab3a53fc6677b22208daa1f1cbd48d795341
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-31 23:34:36 +0300

    Refactored Code.

--- blackjack.py
+++ blackjack.py
@@ -42,6 +42,85 @@ def hand_value_calculation(hand):
         
     return value
 
+def bet(player_money):
+    while True:
+        try:
+            initial_bet = int(input("[GAME] Place your opening bet -> "))
+            if initial_bet > player_money:
+                print(f"[GAME] Your bet exceeds your balance, you currently have {player_money}€!")
+            else:
+                return initial_bet
+        except ValueError:
+            print("[GAME] Your input has to be a valid number, try again.")
+
+def player(player_hand, deck):
+    while True:
+        hand_value = hand_value_calculation(player_hand)
+        print(f"[GAME] Your hand's value is: {hand_value}")
+        if hand_value > 21:
+            print("[GAME] You have busted! Better luck next time.")
+            return False
+        action = input("[GAME] Do you want to 'hit' or 'stand'? -> ")
+        if action == 'hit':
+            player_hand.append(deck.pop())
+            print(f"[GAME] You drew: {player_hand[-1]}")
+        elif action == 'stand':
+            return True
+        else:
+            print("[GAME] Input is invalid, type either 'hit' or 'stand'!")
+
+def dealer(dealer_hand, deck):
+    print(f"[GAME] Dealer reveals his second card... {dealer_hand}")
+    while hand_value_calculation(dealer_hand) < 17:
+        dealer_hand.append(deck.pop())
+        print(f"[GAME] Dealer drew: {dealer_hand[-1]}")
+    return hand_value_calculation(dealer_hand)
+
+def compare(player_hand, dealer_hand, initial_bet, player_money):
+    player_value = hand_value_calculation(player_hand)
+    dealer_value = hand_value_calculation(dealer_hand)
+
+    if dealer_value > 21:
+        print("[GAME] Dealer has busted! You win!")
+        return player_money + (2 * initial_bet)
+    elif dealer_value > player_value:
+        print("[GAME] Dealer wins! Better luck next time.")
+    elif dealer_value < player_value:
+        print("[GAME] You win!")
+        return player_money + initial_bet
+
+    return player_money
+
+def game(player_money, deck):
+    while player_money > 0:
+        if len(deck) < 20: # If deck has less than 20 cards.
+            print("[GAME] Deck needs resetting...\n")
+            deck = deck_creation
+
+        initial_bet = bet(player_money)
+        player_money -= initial_bet
+
+        # Initial Cards #
+        player_hand = [deck.pop(), deck.pop()]
+        dealer_hand = [deck.pop(), deck.pop()]
+
+        # Table - Dealer shows only shows one card #
+        print(f"[GAME] Your cards: {player_hand[0]}, {player_hand[1]}")
+        print(f"[GAME] Dealer's cards: {dealer_hand[0]}\n")
+
+        # Simulate Round #
+        if player(player_hand, deck):
+            dealer_value = dealer(dealer_hand, deck)
+            player_money = compare(player_hand, dealer_hand, initial_bet, player_money)
+
+        print(f"[GAME] Your current balance is {player_money}€.")
+
+        replay = input("[GAME] Do you want to play again? (yes/no) -> ").lower()
+        if replay != 'yes':
+            break
+            
+    print("[GAME] Thank you for playing! Exiting...")
+
 # Main #
 def main():
     # Variables #
@@ -54,76 +133,12 @@ def main():
         choice = input("[GAME] What do you want to do? -> ")
         
         if choice == '1': # Play.
-            if len(deck) < 15: # Deck needs reset.
-                print("[GAME] Deck needs resetting...\n")
-                deck = deck_creation
-        
-            # Bet and Balance #
-            initial_bet = int(input("[GAME] Place your opening bet: "))
-            if initial_bet > player_money:
-                print(f"[GAME] Your bet exceeds your balance, you currently have {player_money}€!")
-                continue
-            else:
-                player_money -= initial_bet
-                print(f"[GAME] You placed {initial_bet}€ and your remaining balance is {player_money}€.")
-            
-            # Initial Cards #
-            player_hand = [deck.pop(), deck.pop()]
-            dealer_hand = [deck.pop(), deck.pop()]
-            
-            # Table - Dealer shows only shows one card #
-            print(f"[GAME] Your cards: {player_hand[0]}, {player_hand[1]}")
-            print(f"[GAME] Dealer's cards: {dealer_hand[0]}\n")
-            
-            # Player Turn #
-            busted = False
-            while True:
-                hand_value = hand_value_calculation(player_hand)
-                print(f"[GAME] Your hand's value is: {hand_value}")
-                if hand_value > 21:
-                    print(f"[GAME] You have busted!")
-                    busted = True
-                    break
-                action = input("[GAME] Do you wish to 'hit' or 'stand'?").lower()
-                if action == 'hit':
-                    player_hand.append(deck.pop())
-                    print(f"[GAME] You drew: {player_hand[-1]}")
-                elif action == 'stand':
-                    break
-                else:
-                    print("[GAME] Input is invalid, type 'hit' or 'stand' -> .")
-            
-            # Dealer Turn #
-            if not busted: # Dealer can continue if the player is still in.
-                print(f"[GAME] Dealer reveals his card... {dealer_hand[1]}")
-                # Add logic flow #
-                while hand_value_calculation(dealer_hand) < 17:
-                    dealer_hand.append(deck.pop())
-                    print(f"[GAME] Dealer has drew: {dealer_hand[-1]}")
-                d_value = hand_value_calculation(dealer_hand)
-                print(f"[GAME] Dealer's value now is: {d_value}")
-                
-                # Compare Player's and Dealer's hands #
-                if d_value > 21:
-                    print(f"[GAME] Dealer has busted! You win.")
-                    player_money += 2 * initial_bet
-                elif d_value > hand_value:
-                    print("[GAME] Dealer wins! Better luck next.")
-                elif d_value < hand_value:
-                    print("[GAME] You win!")
-                    player_money += 2 * initial_bet
-                else:
-                    print("[GAME] Its a draw.")
-                    player_money += initial_bet
-                
-            # Ask the player if he wishes to play again #
-            replay = input("[GAME] Do you want to play again? (yes/no) -> ").lower()
-            if replay != 'yes':
-                break
+            game(player_money, deck)
         elif choice == '2': # Rules.
             rules()
         elif choice == '0':
             print("[GAME] Thank you for playing! Exiting...")
+            break
         else:
             print("[Game] Your input is invalid, try again.")
 
